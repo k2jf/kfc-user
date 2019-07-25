9063486a3e5fdb0dc5f27a028d4a5baa6caf7fe1
kkstrive <kkstrive0124@gmail.com>
2019-07-25 16:53:29 +0800

    add kfc-auth-user

--- UserManage.vue
+++ UserManage.vue
@@ -74,3 +74,7 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+  @import url(./styles/index.css);
+</style>
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import UserManage from './UserManage.vue'
-import './styles/index.css'
+// import './styles/index.css'
 
 export default UserManage
